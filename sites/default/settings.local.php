bfd10c837cec713ad4d66f223902ffcb4c2cb597
Alistair Martin <2270211+AliMartin@users.noreply.github.com>
2025-09-05 06:29:50 +0100

    Beta 2

---
+++ sites/default/settings.local.php
@@ -0,0 +1,19 @@
+<?php
+
+$settings['update_free_access'] = FALSE;
+$settings['file_private_path'] = DRUPAL_ROOT . '/private-files';
+$settings['file_temp_path'] = DRUPAL_ROOT . '/tmp';
+$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
+
+
+
+$settings['trusted_host_patterns'] = [
+  '^jonathanmooreuk\.com$',
+  '^.+\.jonathanmooreuk\.com$',
+  '^jm25\.hyperluminal\.co\.uk$',
+  '^.+\.jm25\.hyperluminal\.co\.uk$',
+  '^jonathanmooreuk25\.ddev\.site$',
+  '^.+\.jonathanmooreuk25\.ddev\.site$',
+];
+
+
